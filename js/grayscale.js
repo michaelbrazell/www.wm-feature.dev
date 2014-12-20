77dfbd44df9e1062d04fb311db8e8c63dc1fb73c
Mike Murray <michaelbrazell@gmail.com>
2014-12-20 12:44:51 -0500

    First edits for preview

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(42.263718, -71.791116), // WorcesterMag HQ
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
